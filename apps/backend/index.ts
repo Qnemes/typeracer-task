5a803aae4e6e356ce9fd63600110f46f81bcc5bf
Qnemes <qnemes@gmail.com>
2026-03-02 13:25:57 +0100

    -fix attempt to fix broken render deploy

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -3,12 +3,13 @@ import { Server } from "socket.io";
 import { setupListeners } from "./setup-listeners.js";
 
 const PORT = process.env.SERVER_PORT || 4000;
+const corsOrigin = process.env.CORS_ORIGIN || "https://typeracer-task.vercel.app";
 
 const httpServer = createServer();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: corsOrigin,
     methods: ["GET", "POST"],
   },
 });
